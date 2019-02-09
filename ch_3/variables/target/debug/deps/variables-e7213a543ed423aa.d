243cb32c015315949828b1d47660b1ececa8bdee
Sean Chavez <realseanchavez@gmail.com>
2019-02-08 17:00:49 -0800

    mutable variables

---
+++ ch_3/variables/target/debug/deps/variables-e7213a543ed423aa.d
@@ -0,0 +1,5 @@
+/Users/seanchavez/Projects/Rust/the_book/ch_3/variables/target/debug/deps/variables-e7213a543ed423aa: src/main.rs
+
+/Users/seanchavez/Projects/Rust/the_book/ch_3/variables/target/debug/deps/variables-e7213a543ed423aa.d: src/main.rs
+
+src/main.rs:
---
+++ ch_3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/seanchavez/Projects/Rust/the_book/ch_3/variables/target/debug/variables.dSYM: /Users/seanchavez/Projects/Rust/the_book/ch_3/variables/src/main.rs
