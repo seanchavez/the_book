e54260895f9bfa3deaf0685568792d731c15ddd7
Sean Chavez <realseanchavez@gmail.com>
2019-02-08 20:09:57 -0800

    statements and expressions

--- ch_3/functions/src/main.rs
+++ ch_3/functions/src/main.rs
@@ -1,10 +1,21 @@
+// fn main() {
+//     println!("Hello, world!");
+
+//     another_function(5, 6);
+// }
+
+// fn another_function(x: i32, y: i32) {
+//     println!("The value of x is: {}", x);
+//     println!("The value of y is: {}", y);
+// } 
+
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
 
-    another_function(5, 6);
-}
+    let y = {
+        let x = 3;
+        x + 1
+    };
 
-fn another_function(x: i32, y: i32) {
-    println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
 }
