590e67c9699af8edebe44d91336d56e68fda55bd
Sean Chavez <realseanchavez@gmail.com>
2019-02-08 17:12:55 -0800

    shadowing

--- ch_3/variables/src/main.rs
+++ ch_3/variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    let mut x = 5;
+    let  x = 5;
+
+    let x = x + 1;
+
+    let x = x * 2;
+    
     println!("The value of x is: {}", x);
-    x = 6;
-    println!("The value of x is: {}", x);
+   
 }
