fc60435134e57fb08b4e9ee07d63e86fc3d02781
Sean Chavez <realseanchavez@gmail.com>
2019-02-10 15:28:23 -0800

    borrowing

---
+++ ch_4/references/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let s1 = String::from("hello");
+
+    // let len = calculate_length(&s1);
+
+    // println!("The length of {} is {}", s1, len);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &String) {
+    some_string.push_str(", world");
+}
