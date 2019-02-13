a7f2c7ecc019d9c67465491156e1be87b98ab341
Sean Chavez <realseanchavez@gmail.com>
2019-02-12 21:31:26 -0800

    lame

--- ch_4/slices/src/main.rs
+++ ch_4/slices/src/main.rs
@@ -1,13 +1,13 @@
 fn main() {
     let mut s = String::from("hello world");
-    let word = first_word(&s);
+    let word = first_word_a(&s);
 
     s.clear();
 
     println!("the first word is: {}", word);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word_a(s: &String) -> &str {
     let bytes = s.as_bytes();
 
     for(i, &item) in bytes.iter().enumerate() {
