e18ec7acc92e742145c4e34300331f49133acfa1
Sean Chavez <realseanchavez@gmail.com>
2019-02-13 18:29:07 -0800

    associated functions

--- ch_5/rectangles/src/main.rs
+++ ch_5/rectangles/src/main.rs
@@ -6,6 +6,10 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
     } 
