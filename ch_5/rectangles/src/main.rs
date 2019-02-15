bf94e52618a288bd71be84d24797b4009e039a97
Sean Chavez <realseanchavez@gmail.com>
2019-02-14 20:49:29 -0800

    shame

--- ch_5/rectangles/src/main.rs
+++ ch_5/rectangles/src/main.rs
@@ -24,12 +24,15 @@ fn main() {
     let rect2 = Rectangle {width: 10, height: 40};
     let rect3 = Rectangle { width: 60, height: 45 };
 
+    let sq = Rectangle::square(3);
+
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
     
 
     println!("The area of the rectangle is {} square pixels.", rect1.area());
     println!("rect1 is {:#?}", rect1);
+    println!("sq is {:#?}", sq)
 }
 
 // fn area(rectangle: &Rectangle) -> u32 {
