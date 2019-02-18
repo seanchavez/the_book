6304258b57e3cecd6a06be513219d2792cb1f644
Sean Chavez <realseanchavez@gmail.com>
2019-02-18 12:50:29 -0800

    matching with Option<t>

--- ch_6/match_enum/src/main.rs
+++ ch_6/match_enum/src/main.rs
@@ -1,29 +1,47 @@
+
+
 fn main() {
-    value_in_cents(Coin::Quarter(UsState::Alaska));
+    //value_in_cents(Coin::Quarter(UsState::Alaska));
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}", five);
+    println!("{:?}", six);
+    println!("{:?}", none);
 }
 
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-    Arkansas,
-}
+//#[derive(Debug)]
+// enum UsState {
+//     Alabama,
+//     Alaska,
+//     Arkansas,
+// }
 
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UsState),
+// }
 
-fn value_in_cents(coin: Coin) -> u32 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}", state);
-            25
-        }
+// fn value_in_cents(coin: Coin) -> u32 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State quarter from {:?}", state);
+//             25
+//         }
+//     }
+// }
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
+
+
+
